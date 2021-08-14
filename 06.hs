83868177cf75f7a3587f797ce492c51b7d73efc8
Anton Lavrov <anton.lavrov@oracle.com>
2021-08-14 14:39:52 -0700

    day 6

---
+++ 06.hs
@@ -0,0 +1,35 @@
+import Common
+import Data.Array
+import Data.Foldable
+import Data.List
+import Data.Maybe
+import Debug.Trace
+
+main = do
+    content <- readFile "06.input"
+    let mem = parse content
+    -- putStrLn $ show $ mem
+    let mems = iterate realloc mem
+    let Just (i, mem') = firstDup mems
+    putStrLn $ show i
+    let Just i0 = elemIndex mem' mems
+    putStrLn $ show $ i - i0
+
+parse s = listArray (0, length l - 1) l
+    where
+        l = map readInt $ words s
+
+
+realloc mem
+    -- | trace ("\nw = " ++ show w ++ ", m = " ++ show m ++ ", mi = " ++ show mi ++ ", d = " ++ show d ++ ", dd = " ++ show dd ++ ": " ++ show (elems mem)) False = undefined
+    | otherwise = array (bounds mem) [(i, (if i == mi then 0 else v) + d + (if (i - mi - 1) `mod` w < dd then 1 else 0)) | (i, v) <- assocs mem]
+    where
+        -- find maximum and its index
+        (m, mi, _) = Data.Foldable.foldl (\(m, i, j) v -> if v > m then (v, j, j+1) else (m, i, j+1)) (0, 0, 0) mem
+        (l, h) = bounds mem
+        w = h - l + 1
+        --  how much bump should each element get
+        d = m `div` w
+        --  last index of the element which gets more bump
+        dd = m `mod` w
+    
--- Common.hs
+++ Common.hs
@@ -1,3 +1,13 @@
 module Common where
+import qualified Data.Set
 
 readInt s = read s :: Int
+
+
+firstDup l = firstDup' l Data.Set.empty 0
+    where
+        firstDup' [] _ _ = Nothing
+        firstDup' (x : xs) xs' i
+            | Data.Set.member x xs' = Just (i, x)
+            | otherwise = firstDup' xs (Data.Set.insert x xs') (i + 1)
+
